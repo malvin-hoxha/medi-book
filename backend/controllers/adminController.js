3e155dfca1324967194aafb9a4dd6a5d0997abed
malvin-hoxha <malvinhodev@gmail.com>
2026-07-21 00:03:08 +0300

    feat: add admin doctor creation route

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,8 +1,65 @@
-import express from 'express';
-import upload from '../middlewares/muller';
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from '../models/doctorModel.js';
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
-import { addDoctor } from './doctorController';
+const addDoctor = async (req, res) => {
+    try {
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
 
-const adminRouter = express.Router();
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
 
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor);
+        if(!validator.isEmail(email)) {
+            return res.json({success: false, message: "PLease enter a valid email"});
+        }
+
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Password must be at least 8 characters" });
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        const imageUrl = imageUpload.secure_url;
+
+        const doctorData = {
+            name,
+            email,
+            image: imageUrl,
+            password: hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address: JSON.parse(address),
+            date: Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        await newDoctor.save()
+        res.json({ success: true, message: 'Doctor Added' });
+    } catch(error) {
+        console.log(error)
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+const loginAdmin = async (req, res) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+export {addDoctor}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 connectDB();
 connectCloudinary();
+
+app.use('/api/admin', adminRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
