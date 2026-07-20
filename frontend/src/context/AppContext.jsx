6f874863e13fcb24d5106d4bff27e33783b2411f
malvin-hoxha <malvinhodev@gmail.com>
2026-07-20 12:53:00 +0300

    feat: add app context for doctors data

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import { doctors } from '../assets/assets'
+
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+
+    const value = {
+        doctors
+    }
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
